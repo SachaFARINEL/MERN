a6660987c72b2c5c8f04acfc988c3594fc9b4d1c
Sacha FARINEL <97121186+SachaFARINEL@users.noreply.github.com>
2023-01-18 12:34:11 +0100

    feat(tests) : add some tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,32 +1,44 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
-const appRequest = request('http://localhost:3500')
 const app = require('../server')
 const User = require('../models/User')
 require("dotenv").config();
 
+
 // Connecting to the database before each test.
-beforeEach(async () => {
+beforeAll(async () => {
     await mongoose.connect(process.env.DATABASE_TEST_URI);
 });
 
 // Closing database connection after each test.
-afterEach(async () => {
+afterAll(async () => {
     await mongoose.connection.close();
 });
+
 describe('Sanity test', () => {
     test('1 should equal 1', () => {
         expect(1).toBe(1)
     })
 })
 
-describe("DELETE /users", () => {
-    it("should delete a user", async () => {
-        const user = await User.findOne({username: 'Sacha'}).lean().exec()
-        const res = await request(app).delete("/users").send({
-            _id: user._id.toString()
+
+describe("GET /users on empty {collection}", () => {
+    it("should return 'No users found'", async () => {
+        const res = await request(app).get("/users");
+        expect(res.statusCode).toBe(400);
+        expect(res.body.message).toBe('No users found');
+    });
+});
+
+describe("POST /users without all fields", () => {
+    it("should create a user", async () => {
+        const res = await request(app).post("/users").send({
+            username: "Sacha",
+            password: "!Hb12345"
         });
-        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).toBe(400);
+        expect(res.body.message).toBe("All fields are required");
+
     });
 });
 
@@ -48,5 +60,15 @@ describe("GET /users", () => {
         const res = await request(app).get("/users");
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBeGreaterThan(0);
+    });
+});
+
+describe("DELETE /users", () => {
+    it("should delete a user", async () => {
+        const user = await User.findOne({username: 'Sacha'}).lean().exec()
+        const res = await request(app).delete("/users").send({
+            _id: user._id.toString()
+        });
+        expect(res.statusCode).toBe(200);
     });
 });
